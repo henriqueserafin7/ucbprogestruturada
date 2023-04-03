223d9ad805280e8af1c28af4914c0eb99da6ec0e
henriqueserafin7 <44422639+henriqueserafin7@users.noreply.github.com>
2023-04-03 15:36:12 -0300

    Add files via upload

---
+++ sequencial/1.5.c
@@ -0,0 +1,15 @@
+#include<stdint.h>
+#include<stdio.h>
+
+int main(){
+    int A, B;
+
+    printf(" Digite valor de A:");
+    scanf("%d", &A);
+
+    printf(" Digite valor de B:");
+    scanf("%d", &B);
+
+   printf("%d",A+B);
+
+}
