b7bcbbbf61494237e46121dbaf72b1514560f796
henriqueserafin7 <44422639+henriqueserafin7@users.noreply.github.com>
2023-04-03 15:38:13 -0300

    Delete sequencial directory

--- sequencial/1.2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-
-printf("Caderno de poesias é um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\nEu falo em forma de versos para todos poderem escutar.\n Agora você já sabe por que os poetas passam os dias escrevendo em seus cadernos de poesias.");
-
-}
